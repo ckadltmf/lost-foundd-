282c6650dbfa43de6d90f33182c70e586d463903
nivtal9 <nivtal9@gmail.com>
2020-12-07 23:05:56 +0200

    upload photos in forms

--- app/src/main/java/com/example/myapplication2/Form.java
+++ app/src/main/java/com/example/myapplication2/Form.java
@@ -2,94 +2,92 @@ package com.example.myapplication2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class Form extends AppCompatActivity  {
     public static final String TAG = "TAG";
-    EditText mObject;
+    EditText mObject, mDate;
     Button mSubmit;
-    FirebaseFirestore fStore;
     Spinner Happened_spinner, Category_spinner;
-    //FirebaseAuth fAuth;
+    ImageView ObjectImage;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    StorageReference storageReference;
+    String DR;
+    Uri imageUri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form);
-        //Intent data = getIntent();
-        Happened_spinner = /*(Spinner)*/findViewById(R.id.spinner1);
-        Category_spinner = /*(Spinner)*/findViewById(R.id.spinner2);
+        storageReference = FirebaseStorage.getInstance().getReference();
+        Happened_spinner = findViewById(R.id.spinner1);
+        Category_spinner = findViewById(R.id.spinner2);
         ArrayAdapter<CharSequence> adapter= ArrayAdapter.createFromResource(this, R.array.Whathappened, R.layout.support_simple_spinner_dropdown_item);
         Happened_spinner.setAdapter(adapter);
         ArrayAdapter<CharSequence> adapter2= ArrayAdapter.createFromResource(this, R.array.Category, R.layout.support_simple_spinner_dropdown_item);
         Category_spinner.setAdapter(adapter2);
         mObject = findViewById(R.id.ObjectName);
+        mDate =findViewById(R.id.TextDate);
         mSubmit = findViewById(R.id.submit);
+        ObjectImage = findViewById(R.id.ObjectImageView);
         fStore = FirebaseFirestore.getInstance();
-        //fAuth = FirebaseAuth.getInstance();
+        fAuth = FirebaseAuth.getInstance();
         mSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                String object= mObject.getText().toString().trim();
                 String happened= Happened_spinner.getSelectedItem().toString();
                 String category= Category_spinner.getSelectedItem().toString();
-                String object=mObject.getText().toString().trim();
-                //String fullName = mFullName.getText().toString();
-                //final String phone = mPhone.getText().toString();
-                //String email=mEmail.getText().toString().trim();
-
-/*                if(TextUtils.isEmpty(email)){
-                    mEmail.setError("Email Fill is required");
-                    return;
-                }
-
+                String userID=fAuth.getCurrentUser().getUid();
                 if(TextUtils.isEmpty(object)){
-                    mFullName.setError("Name Fill is required");
-                    return;
-                }
-                if(TextUtils.isEmpty(object)){
-                    mPhone.setError("Phone Fill is required");
-                    return;
-                }
-                */
-                if(TextUtils.isEmpty(object)){
-                    mObject.setError("Object Fill is required");
+                    mObject.setError("Object Title is required");
                     return;
                 }
                 if(happened.equals("Choose Lost or Found")){
                     ((TextView)Happened_spinner.getSelectedView()).setError("");
-                    //Happened_spinner.setError("Please choose Lost *or* Found");
                     return;
                 }
                 if(category.equals("Choose Category")){
                     ((TextView)Category_spinner.getSelectedView()).setError("");
-                    //Category_spinner.setError("Category choosing is required");
                     return;
                 }
                             Map<String,Object> forms = new HashMap<>();
-                            //forms.put("fName",fullName);
-                            //forms.put("email",email);
-                            //forms.put("phone",phone);
-                            forms.put("object",object);
+                            forms.put("UserID",userID);
+                            forms.put("Object Title",object);
+                            forms.put("Lost/Found",happened);
+                            forms.put("Category",category);
                             fStore.collection("forms").add(forms).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
                                 @Override
                                 public void onSuccess(DocumentReference documentReference) {
                                     Toast.makeText(Form.this,"Your object added Successfully",Toast.LENGTH_SHORT).show();
+                                    DR=documentReference.getId();
+                                    uploadImageToFirebase(imageUri);
                                 }
                             }).addOnFailureListener(new OnFailureListener() {
                                 @Override
@@ -101,20 +99,43 @@ public class Form extends AppCompatActivity  {
             }
 
         });
-/*        userId = fAuth.getCurrentUser().getUid();
-        DocumentReference documentReference = fStore.collection("users").document(userId);
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+        ObjectImage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                if(documentSnapshot.exists()){
-                    mPhone.setText(documentSnapshot.getString("phone"));
-                    mFullName.setText(documentSnapshot.getString("fName"));
-                    mEmail.setText(documentSnapshot.getString("email"));
-
-                }else {
-                    Log.d("tag", "onEvent: Document do not exists");
-                }
+            public void onClick(View v) {
+                Intent openGalleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(openGalleryIntent,1000);
             }
-        });*/
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @androidx.annotation.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                imageUri = data.getData();
+            }
+        }
+
+    }
+    private void uploadImageToFirebase(Uri imageUri) {
+        // uplaod image to firebase storage
+        final StorageReference fileRef = storageReference.child("forms/"+DR+"/ObjectIMG.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(ObjectImage);
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(), "Failed.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
