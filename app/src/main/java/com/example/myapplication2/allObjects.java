f0360e306957fbb6e2b8e7b197a9aa45617c4478
avi130 <avraham836@gmail.com>
2020-12-10 09:47:41 +0200

    listView fix

--- app/src/main/java/com/example/myapplication2/allObjects.java
+++ app/src/main/java/com/example/myapplication2/allObjects.java
@@ -1,65 +1,143 @@
 package com.example.myapplication2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 import java.util.ArrayList;
 
 public class allObjects extends AppCompatActivity {
-    FirebaseAuth fAuth;
-    FirebaseFirestore fStore;
-    FirebaseUser user;
+//    FirebaseAuth fAuth;
+//    FirebaseFirestore fStore;
+//    FirebaseUser user;
 
-    ListView listView;
-    ArrayList<objectData> arrayList = new ArrayList<>();
+ //   ListView listView;
+ //   ArrayList<objectData> arrayList = new ArrayList<>();
+ //   MyAdapter adapter;
+///////
+    ArrayList<objectData> arrayList2 = new ArrayList<>();
     MyAdapter adapter;
+    FirebaseDatabase FBDB;
+    DatabaseReference DBRF;
+    DatabaseReference databaseReference;
+    ListView listView;
+    ArrayList<String> arrayList= new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+    String ObjectTitle;
+    String Description;
+    String ObjectType;
+    int count;
+
+    private  String actObject[]={"Mobile","Jewel","Clothing","Pet","Electronics","Car","Bike","Bag","Glasses","jewel"};
+    private  String happend[]={"Lost","Found"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.all_objects);
+        setContentView(R.layout.activity_new_show_report);
 
 
-        listView = findViewById(R.id.listView);
-        fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
+        listView = findViewById(R.id.listViewTxt);
+        FBDB= FirebaseDatabase.getInstance();
+        DBRF=FBDB.getReference("forms");
+        count=1;
 
-        fStore.collection("forms")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "blabla data!!!", Toast.LENGTH_LONG).show();
-                            int i=1;
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                arrayList.add(new objectData(i+") item: "+ document.get("Category").toString(),"Category: "+ document.get("Category").toString() ,"Description: "+ document.get("Object Title").toString() ));
-                                 i++;
+    for (String object:actObject) {
+            databaseReference = FirebaseDatabase.getInstance().getReference("forms").child("Found").child(object);
+            listView = (ListView) findViewById(R.id.listViewTxt);
+            arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+            listView.setAdapter(arrayAdapter);
+            databaseReference.addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-                            }
-                        } else {
-                          //  Log.d("TAG", "Error getting documents: ", task.getException());
-                          Toast.makeText(getApplicationContext(), "Error getting data!!!", Toast.LENGTH_LONG).show();
+                    ObjectTitle = String.valueOf(dataSnapshot.child("Object Title").getValue());
+                    Description = String.valueOf(dataSnapshot.child("description").getValue());
+                    ObjectType = object;
 
-                        }
-                        adapter = new MyAdapter(allObjects.this, arrayList);
-                        listView.setAdapter(adapter);
-                    }
-                });
+                    arrayList2.add(new objectData(count + ") Object Title: " + ObjectTitle, " Object type: " + ObjectType, "Description: " + Description));
+                    adapter = new MyAdapter(allObjects.this, arrayList2);
+                    listView.setAdapter(adapter);
+                    count++;
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        listView = findViewById(R.id.listView);
+//        fAuth = FirebaseAuth.getInstance();
+//        fStore = FirebaseFirestore.getInstance();
+//
+//        fStore.collection("forms")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(getApplicationContext(), "blabla data!!!", Toast.LENGTH_LONG).show();
+//                            int i=1;
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                arrayList.add(new objectData(i+") item: "+ document.get("Category").toString(),"Category: "+ document.get("Category").toString() ,"Description: "+ document.get("Object Title").toString() ));
+//                                 i++;
+//
+//                            }
+//                        } else {
+//                          //  Log.d("TAG", "Error getting documents: ", task.getException());
+//                          Toast.makeText(getApplicationContext(), "Error getting data!!!", Toast.LENGTH_LONG).show();
+//
+//                        }
+//                        adapter = new MyAdapter(allObjects.this, arrayList);
+//                        listView.setAdapter(adapter);
+//                    }
+//                });
 
 //        arrayList.add(new objectData(12, " Niyaz","65757657657"));
 //       // adapter = new MyAdapter(this, arrayList);
--- app/src/main/java/com/example/myapplication2/all_reports.java
+++ app/src/main/java/com/example/myapplication2/all_reports.java
@@ -1,9 +1,12 @@
 package com.example.myapplication2;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -11,6 +14,12 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
@@ -19,45 +28,118 @@ import java.util.ArrayList;
 
 public class all_reports extends AppCompatActivity {
 
-    FirebaseAuth fAuth;
-    FirebaseFirestore fStore;
-    FirebaseUser user;
+    //FirebaseAuth fAuth;
+    //FirebaseFirestore fStore;
+   // FirebaseUser user;
 
-    ListView listView;
-    ArrayList<ReportData> arrayList = new ArrayList<>();
+
+    ArrayList<ReportData> arrayList2 = new ArrayList<>();
     MyReportAdapter adapter;
+    FirebaseDatabase FBDB;
+    DatabaseReference DBRF;
+    //////////
+    DatabaseReference databaseReference;
+    ListView listView;
+    ArrayList<String> arrayList= new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+    String UserID;
+    String Description;
+    String ReportType;
+    int count;
+    private  String actReport[]={"Scam","Fake advertise","inappropriate","harassment","other"};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.all_objects);
+      //  setContentView(R.layout.all_objects);
+        setContentView(R.layout.activity_new_show_report);
+
+        listView = findViewById(R.id.listViewTxt);
+        FBDB= FirebaseDatabase.getInstance();
+        DBRF=FBDB.getReference("report");
+        count=1;
 
 
-        listView = findViewById(R.id.listView);
-        fAuth = FirebaseAuth.getInstance();
-        fStore = FirebaseFirestore.getInstance();
+for (String reportSubject:actReport) {
+    databaseReference = FirebaseDatabase.getInstance().getReference("report").child(reportSubject);
+    listView = (ListView) findViewById(R.id.listViewTxt);
+    arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+    listView.setAdapter(arrayAdapter);
+    databaseReference.addChildEventListener(new ChildEventListener() {
+        @Override
+        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-        fStore.collection("report")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(getApplicationContext(), "blabla data!!!", Toast.LENGTH_LONG).show();
-                            int i=1;
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                arrayList.add(new ReportData(i+") UserID: "+ document.get("UserID").toString(),"Report Subject: "+ document.get("Report Subject").toString() ,"Description: "+ document.get("Description").toString() ));
-                                i++;
+            UserID = String.valueOf(dataSnapshot.child("UserID").getValue());
+            Description = String.valueOf(dataSnapshot.child("Description").getValue());
+            ReportType = reportSubject;
 
-                            }
-                        } else {
-                            //  Log.d("TAG", "Error getting documents: ", task.getException());
-                            Toast.makeText(getApplicationContext(), "Error getting data!!!", Toast.LENGTH_LONG).show();
+            arrayList2.add(new ReportData(count+") UserID: "+ UserID," Report type: "+ReportType ,"Description: "+ Description ));
+       //     arrayAdapter.add(count+") Report type: "+ReportType);
+     //       arrayAdapter.add("UID is "+UserID );
 
-                        }
-                        adapter = new MyReportAdapter(all_reports.this, arrayList);
-                        listView.setAdapter(adapter);
-                    }
-                });
+          //  arrayAdapter.add("DESCRIPTION:  "+Description );
+         //   arrayAdapter.notifyDataSetChanged();
+            adapter = new MyReportAdapter(all_reports.this, arrayList2);
+            listView.setAdapter(adapter);
+            count++;
+        }
+
+        @Override
+        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+        }
+
+        @Override
+        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+        }
+    });
+}
+
+
+
+//        listView = findViewById(R.id.listView);
+//        fAuth = FirebaseAuth.getInstance();
+//        fStore = FirebaseFirestore.getInstance();
+//
+//        FBDB= FirebaseDatabase.getInstance();
+//        DBRF=FBDB.getReference("report");
+//        DBRF = FBDB.getReference("/report");
+//        DBRF.addValueEventListener(changeListener);
+//
+//        fStore.collection("report")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(getApplicationContext(), "blabla data!!!", Toast.LENGTH_LONG).show();
+//                            int i=1;
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                arrayList.add(new ReportData(i+") UserID: "+ document.get("UserID").toString(),"Report Subject: "+ document.get("Report Subject").toString() ,"Description: "+ document.get("Description").toString() ));
+//                                i++;
+//
+//                            }
+//                        } else {
+//                            //  Log.d("TAG", "Error getting documents: ", task.getException());
+//                            Toast.makeText(getApplicationContext(), "Error getting data!!!", Toast.LENGTH_LONG).show();
+//
+//                        }
+//                        adapter = new MyReportAdapter(all_reports.this, arrayList);
+//                        listView.setAdapter(adapter);
+//                    }
+//                });
 
 //        arrayList.add(new objectData(12, " Niyaz","65757657657"));
 //       // adapter = new MyAdapter(this, arrayList);
